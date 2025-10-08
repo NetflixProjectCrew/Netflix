8691884468eb50f03f624926eac4ff4decdb1b80
MiuselmianMV <m.maksim2603@icloud.com>
2025-10-08 06:13:09 +0300

    add urls views serializers for accounts app

--- Backend/netflixBack/urls.py
+++ Backend/netflixBack/urls.py
@@ -1,8 +1,14 @@
 #Главный файл маршрутизации проекта
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/v1/", include("apps.accounts.urls")),
+    path("api/v1/auth/", include("apps.accounts.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
