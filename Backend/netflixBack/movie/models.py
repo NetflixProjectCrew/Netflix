c0b6fd6e53071cdafab8daae772b25c019a6b46e
V-Sitnikoff747 <78319565+V-Sitnikoff747@users.noreply.github.com>
2025-10-10 11:45:50 +0300

    Добавлены новые поля в модель Movie и обновлён admin.py, добавлен .gitignore

--- Backend/netflixBack/movie/models.py
+++ Backend/netflixBack/movie/models.py
@@ -1,11 +1,30 @@
 from django.db import models
 
+class Genre(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+    
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    bio = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Movie(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     year = models.IntegerField()
     poster = models.ImageField(upload_to='posters/', blank=True, null=True)
     video = models.FileField(upload_to='movies/', blank=True, null=True)
+
+    likes = models.PositiveIntegerField(default=0)
+    genres = models.ManyToManyField(Genre, related_name='movies', blank=True)
+    authors = models.ManyToManyField(Author, related_name='movies', blank=True)
 
     def __str__(self):
         return self.title
